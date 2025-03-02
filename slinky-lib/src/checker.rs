d7612c081e45f655ab0e0d817f04d92f1869ccf5
Théophile Cantelobre <theophilec@gmail.com>
2025-03-02 10:50:07 +0100

    Refactoring check_link

--- slinky-lib/src/checker.rs
+++ slinky-lib/src/checker.rs
@@ -1,10 +1,20 @@
-use reqwest::blocking::Client;
+use reqwest::{blocking::Client, StatusCode};
 
 #[derive(Debug, PartialEq)]
 pub enum CheckResult {
     Pass,
     Fail,
-    Unknown,
+}
+
+pub fn better_check_link(url: &str, client: &Client) -> CheckResult {
+    client
+        .get(url)
+        .send()
+        .map_or(CheckResult::Fail, |r| match r.status() {
+            StatusCode::OK => CheckResult::Pass,
+            StatusCode::NOT_FOUND => CheckResult::Fail,
+            _ => CheckResult::Fail,
+        })
 }
 
 pub fn check_link(url: &str, client: &Client) -> CheckResult {
@@ -19,7 +29,7 @@ pub fn check_link(url: &str, client: &Client) -> CheckResult {
                 CheckResult::Fail
             }
         }
-        Err(_) => CheckResult::Unknown,
+        Err(_) => CheckResult::Fail,
     }
 }
 
@@ -27,20 +37,16 @@ pub fn check_link(url: &str, client: &Client) -> CheckResult {
 fn test_check_link_pass() {
     let client = Client::new();
     let result_1 = check_link("https://www.example.com", &client);
-    let result_2 = check_link("https://www.example.com", &client);
-    assert_eq!(result_2, CheckResult::Pass);
+    let result_2 = better_check_link("https://www.example.com", &client);
     assert_eq!(result_1, CheckResult::Pass);
+    assert_eq!(result_1, result_2);
 }
 
 #[test]
 fn test_check_link_fail() {
     let client = Client::new();
-    let result = check_link("https://www.exampl239048230894e.com", &client);
-    assert_eq!(result, CheckResult::Fail);
+    let result_1 = check_link("https://www.exampl239048230894e.com", &client);
+    let result_2 = better_check_link("https://www.exampl239048230894e.com", &client);
+    assert_eq!(result_1, CheckResult::Fail);
+    assert_eq!(result_1, result_2);
 }
-
-// #[test]
-// fn test_check_link_unknown() {
-//     let result = check_link("https://www.linkedin.com");
-//     assert_eq!(result, CheckResult::Unknown);
-// }
