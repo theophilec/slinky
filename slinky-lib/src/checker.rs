acdb609a12545d36fac93facdb4c4752b2fd3507
Théophile Cantelobre <theophilec@gmail.com>
2025-03-02 10:50:46 +0100

    Changed function names

--- slinky-lib/src/checker.rs
+++ slinky-lib/src/checker.rs
@@ -6,7 +6,7 @@ pub enum CheckResult {
     Fail,
 }
 
-pub fn better_check_link(url: &str, client: &Client) -> CheckResult {
+pub fn check_link(url: &str, client: &Client) -> CheckResult {
     client
         .get(url)
         .send()
@@ -17,36 +17,16 @@ pub fn better_check_link(url: &str, client: &Client) -> CheckResult {
         })
 }
 
-pub fn check_link(url: &str, client: &Client) -> CheckResult {
-    let response = client.get(url).send();
-
-    match response {
-        Ok(response) => {
-            if response.status().is_success() {
-                CheckResult::Pass
-            } else {
-                println!("Failed to fetch URL: {}", response.status());
-                CheckResult::Fail
-            }
-        }
-        Err(_) => CheckResult::Fail,
-    }
-}
-
 #[test]
 fn test_check_link_pass() {
     let client = Client::new();
     let result_1 = check_link("https://www.example.com", &client);
-    let result_2 = better_check_link("https://www.example.com", &client);
     assert_eq!(result_1, CheckResult::Pass);
-    assert_eq!(result_1, result_2);
 }
 
 #[test]
 fn test_check_link_fail() {
     let client = Client::new();
     let result_1 = check_link("https://www.exampl239048230894e.com", &client);
-    let result_2 = better_check_link("https://www.exampl239048230894e.com", &client);
     assert_eq!(result_1, CheckResult::Fail);
-    assert_eq!(result_1, result_2);
 }
