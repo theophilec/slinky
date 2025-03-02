d7e9be392c4028a213e00849cc57aec8fab1481f
Théophile Cantelobre <theophilec@gmail.com>
2025-03-02 17:56:12 +0100

    WIP commit

--- slinky-lib/src/checker.rs
+++ slinky-lib/src/checker.rs
@@ -17,6 +17,47 @@ pub fn check_link(url: &str, client: &Client) -> CheckResult {
         })
 }
 
+fn get_check_link(url: &str, client: &Client) -> (CheckResult, Option<String>) {
+    client
+        .get(url)
+        .send()
+        .map_or((CheckResult::Fail, None), |r| match r.status() {
+            StatusCode::OK => (CheckResult::Pass, r.text().map_or(None, |text| Some(text))),
+            StatusCode::NOT_FOUND => (CheckResult::Fail, None),
+            _ => (CheckResult::Fail, None),
+        })
+}
+
+pub fn check_link_content(url: &str, client: &Client, required_text: &str) -> CheckResult {
+    let (result, content) = get_check_link(url, client);
+    match (result, content) {
+        (CheckResult::Fail, _) => CheckResult::Fail,
+        (CheckResult::Pass, None) => CheckResult::Pass,
+        (CheckResult::Pass, Some(content)) => {
+            if content.contains(required_text) {
+                CheckResult::Pass
+            } else {
+                CheckResult::Fail
+            }
+        }
+    }
+}
+
+pub fn not_check_link_content(url: &str, client: &Client, required_text: &str) -> CheckResult {
+    let (result, content) = get_check_link(url, client);
+    match (result, content) {
+        (CheckResult::Fail, _) => CheckResult::Fail,
+        (CheckResult::Pass, None) => CheckResult::Pass,
+        (CheckResult::Pass, Some(content)) => {
+            if content.contains(required_text) {
+                CheckResult::Fail
+            } else {
+                CheckResult::Pass
+            }
+        }
+    }
+}
+
 #[test]
 fn test_check_link_pass() {
     let client = Client::new();
